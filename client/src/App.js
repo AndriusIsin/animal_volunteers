0a4d316fc5479dad634fcc5054d12f62662a40b3
AndriusIsin <isin.andrius@gmail.com>
2023-10-20 15:59:41 +0100

    calendar done first draft

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,23 @@ import { Grid } from "@mui/material";
 
 import Navbar from "./Components/Navbar";
 import { Outlet } from "react-router-dom";
+import Calendar from "./Components/Calendar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-    <MainBanner />
-      <Grid className="container" container mt="3rem" justifyContent="space-between" alignItems="center">
-        <Grid item xs={5}>Calendar</Grid>
+      <MainBanner />
+      <Grid
+        className="container"
+        container
+        mt="3rem"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid item xs={5}>
+          <Calendar />
+        </Grid>
         <Grid item xs={7}>
           <InputForm />
         </Grid>
