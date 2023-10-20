1ce11c1fede9718bfb4ab67400ea28755fa48dbb
Olha Danylevska <odanilevskaya2@gmail.com>
2023-10-20 13:20:41 +0100

    added another endpoint

--- Animal_volunteers/server/server.js
+++ Animal_volunteers/server/server.js
@@ -25,7 +25,7 @@ const pool = new Pool({
 //   }
 // });
 
-
+//get everything from sessions table with the name of the volunteer
 app.get("/sessions", async (req, res) => {
   try {
     const query = `
@@ -63,6 +63,87 @@ app.get("/volunteers", async (req, res) => {
   }
 });
 
+
+//create new volunteer
+
+app.post("/volunteers", (req, res) => {
+  let newName = req.body.name;
+  let newPhone = req.body.phone;
+  let newEmail = req.body.email;
+
+  if (!newName) {
+    res.status(400).send("Name field is missing");
+  } else if (!newPhone) {
+    res.status(400).send("Phone field is missing");
+  } else if (!newEmail) {
+    res.status(400).send("Email field is missing");
+  } else {
+    const query = `
+      INSERT INTO volunteers (Name, Phone, Email)
+      VALUES ($1, $2, $3)
+    `;
+
+    pool.query(query, [newName, newPhone, newEmail])
+      .then(() => {
+        res.status(200).send("New volunteer created");
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send("Error creating a new volunteer");
+      });
+  }
+});
+
+
+app.post("/volunteers-and-sessions", async (req, res) => {
+  const {
+    name,
+    phone,
+    email,
+    date,
+    time
+  } = req.body;
+
+  if (!name || !phone || !email || !date || !time) {
+    res.status(400).send("Incomplete data provided");
+    return;
+  }
+
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+
+    // Insert the new volunteer into the "volunteers" table
+    const volunteerInsertQuery = `
+      INSERT INTO volunteers (Name, Phone, Email)
+      VALUES ($1, $2, $3)
+      RETURNING id;
+    `;
+
+    const volunteerValues = [name, phone, email];
+    const volunteerResult = await client.query(volunteerInsertQuery, volunteerValues);
+    const volunteerId = volunteerResult.rows[0].id;
+
+    // Insert the new session into the "sessions" table
+    const sessionInsertQuery = `
+      INSERT INTO sessions (Date, Time, Volunteers_id)
+      VALUES ($1, $2, $3);
+    `;
+
+    const sessionValues = [date, time, volunteerId];
+    await client.query(sessionInsertQuery, sessionValues);
+
+    await client.query("COMMIT");
+    res.status(201).send("New volunteer and session created successfully");
+  } catch (err) {
+    await client.query("ROLLBACK");
+    console.error("Error creating volunteer and session:", err);
+    res.status(500).send("An error occurred while creating volunteer and session");
+  } finally {
+    client.release();
+  }
+});
 
 
 
