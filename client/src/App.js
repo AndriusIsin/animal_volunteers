0392082c734e1bdc03c2109d2a5ccb0284268373
AndriusIsin <isin.andrius@gmail.com>
2023-10-22 10:10:51 +0100

    added calendar and changed script build

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,29 @@ import { Grid } from "@mui/material";
 import Navbar from "./Components/Navbar";
 import { Outlet } from "react-router-dom";
 import AdminVue from "./Components/AdminVue";
+import Calendar from "./Components/Calendar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <MainBanner />
-      <Grid className="container" container mt="3rem" justifyContent="space-between" alignItems="center" sx={{
-        width: {
-          xs: "98%",
-          md: "90%",
-        },
-      }}>
-        <Grid item xs={5}>Calendar</Grid>
+      <Grid
+        className="container"
+        container
+        mt="3rem"
+        justifyContent="space-between"
+        alignItems="center"
+        sx={{
+          width: {
+            xs: "98%",
+            md: "90%",
+          },
+        }}
+      >
+        <Grid item xs={5}>
+          <Calendar />
+        </Grid>
         <Grid item xs={7}>
           <InputForm />
         </Grid>
--- client/src/Components/Calendar.js
+++ client/src/Components/Calendar.js
@@ -1,11 +1,23 @@
-import React from "react";
+import * as React from "react";
+import dayjs from "dayjs";
 
-const Calendar = () => {
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+
+export default function Calendar() {
+  const [value, setValue] = React.useState(dayjs("2022-04-17"));
+  console.log(value);
   return (
-    <div>
-      <p> Calendar</p>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+          console.log(value);
+          console.log(value.$d.toISOString());
+        }}
+      />
+    </LocalizationProvider>
   );
-};
-
-export default Calendar;
+}
